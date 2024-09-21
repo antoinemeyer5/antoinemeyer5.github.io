b790442306da1078987ce7d74e3c4b1cc60ead64
antoinemeyer5 <antoine.meyer54@gmail.com>
2024-09-21 17:58:55 -0400

    Fix part 2

--- js/partTwo.js
+++ js/partTwo.js
@@ -1,6 +1,5 @@
-import * as THREE from 'three'
-
-import { OrbitControls } from './OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
 
 class Cube extends THREE.Mesh {
   constructor(color) {
